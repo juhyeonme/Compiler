a6356a7f9c017359e03e301bacf6a73d4dc5e5c5
juhyeon96 <kjh96905@gmail.com>
2019-05-24 15:55:52 +0900

    initial commit

---
+++ PA1/moretest.c
@@ -0,0 +1,21 @@
+int a = 10;
+float b = 20.234;
+//variables
+switch(a) {
+	case 0123:
+		/*
+		 for compiler
+		 testing
+		 */
+		int c = 0;
+		char ch = '\'';
+		char ch2 = '#';
+		char ch3 = '_';
+		char ch4 = '~';
+		ch4 = '!';
+		ch4 = '|';
+		ch4 = '\"';
+		ch4 = '\t';
+		ch4 = '*';
+		return 0x1Af;
+}
---
+++ PA1/test.c
@@ -0,0 +1,7 @@
+int foo(char ch){
+	int _ab32 = 12;	// Variable
+	_ab32 *= 1.232;
+	if(_ab32 >= 'A')	printf("Hello\n");
+	return _ab32 - 0x10A;
+}
+
